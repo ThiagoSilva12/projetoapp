db1af168094d59f1a226fe3940c7b9e1ec38ab1d
ThiagoSilva12 <79534413+ThiagoSilva12@users.noreply.github.com>
2021-12-03 09:10:00 -0300

    Update Paciente.tsx

--- screen/Paciente.tsx
+++ screen/Paciente.tsx
@@ -16,3 +16,40 @@ const[senha,setSenha] =React.useState("");
         </View>
     )
 }
+    <View style={styles.container}>
+        <Image source={require("")} style={styles.imglogo}/
+        <View style={styles.contoles}>
+   <TextInput placeholder="Nome" style={styles.input}
+        value={nome}
+       onChanceText={(value)=>setNome(value)}
+    
+       />
+        
+      <TextInput placeholder="Senha" secureTextEntry style={styles.input}
+          value={senha}
+          onChanceText={(value)=>setSenha(value)}
+          
+          />
+         <TouchableOpacity style={styles.btnatendimeto} onPress={()=>{
+                nome=nome;
+                senha=senha;  
+                let retorno = efetuarAtendimento;
+                
+              if (retorno[0] == "Atendido"){
+                navigation.navigate("Home", {dados:retorno});
+              }
+            }};  
+            <Text style={styles.txtbtnpaciente>Paciente </Text> 
+                </TouchableOpacity>
+            </View>
+             
+            <TouchableOpacity style={styles.txtbtnatendimento} onPress={()=>{             
+                 <navigation.navigate("Atendido")        
+               }}>          
+               <Text style={styles.txtbtnatendimento>Atendimento</Text> 
+                        </TouchableOpacity>
+                </View>
+          )
+}          
+                
+                
