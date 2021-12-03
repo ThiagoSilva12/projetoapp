3366912196ca21e62fa8158f68ae2660129b73ae
ThiagoSilva12 <79534413+ThiagoSilva12@users.noreply.github.com>
2021-12-03 10:59:40 -0300

    Update Styles.tsx

--- css/Styles.tsx
+++ css/Styles.tsx
@@ -2,8 +2,42 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
 
-
-
-
+    container:{
+      flex:1,
+    justifyContent:'center',
+        alignItems:"center"
+    },
+    imglogo:{
+        width:100,
+        heigth:200,
+        resideMode:'cover'
+    },
+    controles:{
+width:'80%',
+padding:10,
+margin:20,
+shadowColor:'black',
+shadowOffset:{width:10, height:10},
+shadowOpacity:1,
+shadowRadius:10,
+elevation:2,
+backgroundColor:'white'
+},
+input:{
+    borderBottomColor:'#ddd',
+    borderBottomWidth:1,
+    padding:1,
+    marginBottom:10,
+    fontSize: 15,
+},
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 })
